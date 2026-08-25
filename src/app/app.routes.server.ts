cdf441de242191b7267a8a6916260825f0522310
Muhammad Ahmed Raza <muhammadahmedraza437@gmail.com>
2026-08-25 16:13:56 +0500

    new changes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'app/electronics/:slug',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'app/product/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
